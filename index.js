0892a79174e83614d9b261fe7b8e089c110236c2
LP <lpscrypt@gmail.com>
2020-01-07 13:56:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(musicians, insturments){
+  var print = []
+  for (i=0, i<musicians.length,i++){
+    print.push(`${musicians[i]} plays ${instrument}`)
+  }
+}

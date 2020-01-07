af2e959f144b534be97f6c20dec68a57b60ee5a5
LP <lpscrypt@gmail.com>
2020-01-07 14:00:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 // add solution here
 function theBeatlesPlay(musicians, insturments){
   var print = []
-  for (i=0, i<musicians.length,i++){
+  for (i=0; i<musicians.length; i++){
     print.push(`${musicians[i]} plays ${instrument}`)
   }
+  return print
+}
+
+function johnLennonFacts(array){
+  var i = 0
+  while (array.length>i){
+    array[i] = array[i]+"!!!"
+    i++
+  }
+  return array
 }
